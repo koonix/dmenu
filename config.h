2386b54d937d797b97c71219658dbcbc70d1cf9a
ehsan gn <ehsangn@protonmail.ch>
2021-06-18 14:29:40 +0430

    add pywal support

--- config.h
+++ config.h
@@ -6,14 +6,19 @@ static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = { "peep:size=12" , "Symbols Nerd Font:size=10", "JoyPixels:size=16" };
 static const char *prompt      = NULL; /* -p  option; prompt to the left of input field */
+#if defined __has_include && __has_include ("/tmp/wal-dmenu.h")
+#include "/tmp/wal-dmenu.h"
+#endif
+#ifndef PYWAL
 static const char *colors[SchemeLast][2] = {
-	/* fg, bg */
-	[SchemeNorm] = { "#666666", "#000000" },
-	[SchemeSel]  = { "#aaaaaa", "#252525" },
-	[SchemeSelHighlight]  = { "#aaaaaa", "#252525" },
-	[SchemeNormHighlight] = { "#666666", "#000000" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+    /*                fg         bg      */
+    [SchemeNorm] = { "#666666", "#000000" },
+    [SchemeSel]  = { "#aaaaaa", "#252525" },
+    [SchemeSelHighlight]  = { "#aaaaaa", "#252525" },
+    [SchemeNormHighlight] = { "#666666", "#000000" },
+    [SchemeOut] = { "#000000", "#00ffff" },
 };
+#endif
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
 
