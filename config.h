4e86553c7974723d4018742c36760f67c9a72240
Ehsan Ghorbannezhad <ehsan@disroot.org>
2022-04-02 14:04:56 +0430

    add a farsi font, do some visual cleanup

--- config.h
+++ config.h
@@ -3,36 +3,41 @@
 
 /* which layout to set the keyboard to while dmenu is running. the layout will be
  * restored to it's previous state after dmenu's exit. layouts start from zero.
- * negative values mean disable this feature. */
+ * negative values disable this feature. */
 static int xkblayout = 0;
 
+static int topbar = 1; /* -b option; if 0, dmenu appears at bottom */
+static int fuzzy = 1;  /* -F option; if 0, dmenu doesn't use fuzzy matching */
+static const char *prompt = NULL; /* -p option; prompt to the left of input field */
 static const unsigned int alpha = 0xff;
-static int topbar = 1; /* -b  option; if 0, dmenu appears at bottom */
-static int fuzzy = 1;  /* -F  option; if 0, dmenu doesn't use fuzzy matching */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = { "peep:size=12" , "Symbols Nerd Font:size=10", "JoyPixels:size=16" };
-static const char *prompt      = NULL; /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {
+	"peep:size=12",
+	"Vazirmatn NL:size=10",
+	"Symbols Nerd Font:size=10",
+	"JoyPixels:size=16"
+};
 
 static const char *colors[SchemeLast][2] = {
-    /*                         fg          bg        */
-    [SchemeNorm]           = { "#777777", "#000000"  },
-    [SchemeNormHighlight]  = { "#777777", "#000000"  },
-    [SchemeSel]            = { "white",   "#005577"  },
-    [SchemeSelHighlight]   = { "cyan",    "#005577"  },
-    [SchemeOut]            = { "black",   "darkcyan" },
+	/*                         fg          bg        */
+	[SchemeNorm]           = { "#777777", "#000000"  },
+	[SchemeNormHighlight]  = { "#777777", "#000000"  },
+	[SchemeSel]            = { "white",   "#005577"  },
+	[SchemeSelHighlight]   = { "cyan",    "#005577"  },
+	[SchemeOut]            = { "black",   "darkcyan" },
 };
 
 static const unsigned int alphas[SchemeLast][2] = {
-    [SchemeNorm] = { OPAQUE, alpha },
-    [SchemeSel]  = { OPAQUE, alpha },
-    [SchemeOut]  = { OPAQUE, alpha },
+	[SchemeNorm] = { OPAQUE, alpha },
+	[SchemeSel]  = { OPAQUE, alpha },
+	[SchemeOut]  = { OPAQUE, alpha },
 };
 
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines = 0;
 
-/*
- * Characters not considered part of a word while deleting words
- * for example: " /?\"&[]"
- */
+/* characters not considered part of a word while deleting words; for example: " /?\"&[]" */
 static const char worddelimiters[] = " ";
+
+// vim:noexpandtab
