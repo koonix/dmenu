f91e8c0115b3e9034794178c0d3f1532e017f9f0
ghesy <ehsan@disroot.org>
2022-09-24 17:14:25 +0330

    don't show numbers in password mode

--- dmenu.c
+++ dmenu.c
@@ -192,13 +192,19 @@ recalculatenumbers()
 {
 	unsigned int numer = 0, denom = 0;
 	struct item *item;
+
+	if (passwd)
+		return;
+
 	if (matchend) {
 		numer++;
 		for (item = matchend; item && item->left; item = item->left)
 			numer++;
 	}
+
 	for (item = items; item && item->text; item++)
 		denom++;
+
 	snprintf(numbers, NUMBERSBUFSIZE, "%d/%d", numer, denom);
 }
 
